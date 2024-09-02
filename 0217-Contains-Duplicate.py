9b0d3bb8fd4704939ea52258d9d56594a73c6f0e
Adam Johnson <adamgj3030@gmail.com>
2024-09-02 00:01:40 -0500

    Delete 0217-Contains-Duplicate.py

--- 0217-Contains-Duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        hashmap = set()
-        for num in nums:
-            if num in hashmap:
-                return True
-            hashmap.add(num)
-        return False
