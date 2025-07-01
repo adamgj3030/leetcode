a558605b4f44c064324381aac93d6d6f823c9154
Adam Johnson <adamgj3030@gmail.com>
2025-07-01 13:14:50 -0500

    Update 0032-Longest-Valid-Parentheses.py

--- python/0032-Longest-Valid-Parentheses.py
+++ python/0032-Longest-Valid-Parentheses.py
@@ -26,3 +26,44 @@ class Solution:
             else:
                 stack.append((i, char))
         return max_len
+
+
+
+OR
+
+
+
+'''
+'greedily' use left and right pointers to track longest
+right tracks ')' and left tracks '('
+if right is ever > left, we know the the longest possible has stopped growing
+reset and continue
+when they match, we have '(' == ')' and can see if there is a new max_len
+'''
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        left = right = max_len = 0
+
+        for char in s:
+            if char == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                max_len = max(max_len, 2 * right)
+            elif right > left:
+                left = right = 0
+        
+        left = right = 0
+        for char in reversed(s):
+            if char == ')':
+                right += 1
+            else:
+                left += 1
+            if left == right:
+                max_len = max(max_len, 2 * right)
+            elif left > right:
+                left = right = 0
+        
+        return max_len
+            
